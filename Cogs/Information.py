8cab5b34a097dd428d10314b7099020a72884de1
nnsm522 <jangyc95@naver.com>
2023-01-27 19:00:02 +0900

    삭제 기능 구현, 코드 수정 필요

--- Cogs/Information.py
+++ Cogs/Information.py
@@ -73,7 +73,6 @@ class Information(commands.Cog):
         self.bot = bot
         print("Info Cog loaded")
 
-    @commands.command()
     async def sync(self, ctx):
         fmt = await ctx.bot.tree.sync(guild=ctx.guild)
         await ctx.send(f"Synced {len(fmt)} commands")
@@ -170,6 +169,22 @@ class CreatePeopleModal(discord.ui.Modal, title="정보 등록(일반)"):
         register(file_names[0], discord_id, [discord_name, role, name, birth, school, personal_phone_number, parents_phone_number])
         await interaction.response.send_message(f"{interaction.user}님의 정보가 등록되었습니다.", ephemeral=True)
 
+#삭제, 취소 버튼
+class DeleteButtonView(discord.ui.View):
+    def __init__(self, *, timeout = 180):
+        super().__init__(timeout=timeout)
+    @discord.ui.button(label="삭제", style=discord.ButtonStyle.danger)
+    async def delete_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        id = f"'{interaction.user.id}"
+        for file_name in file_names:
+            dataframe = load_file(file_name)
+            dataframe.drop(id, axis=0, inplace=True)
+            dataframe.to_csv(f"data/{file_name}.csv", encoding='utf-8-sig')
+
+        await interaction.response.send_message("정보가 삭제되었습니다.")
+    @discord.ui.button(label="취소", style=discord.ButtonStyle.grey)
+    async def cancel_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("취소되었습니다.")
 
 async def setup(bot):
     await bot.add_cog(Information(bot))
