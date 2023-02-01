5ce91daf0d95eb89014ff0cee63d1a390127237a
nnsm522 <jangyc95@naver.com>
2023-02-01 16:33:41 +0900

    오류 처리 방식 변경

--- Cogs/Attendance.py
+++ Cogs/Attendance.py
@@ -17,7 +17,10 @@ last_member_data = None
 def give_coin(key:dict):
     db.member_data.update_one(key, {"$inc": {"r_coin": 100}}, True)
 
-
+def is_owner(ctx):
+    return ctx.author == ctx.guild.owner
+def is_class(ctx):
+    return str(ctx.channel) == "수업"
 
 class Attendance(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -25,24 +28,27 @@ class Attendance(commands.Cog):
         print("Attendance Cog loaded")
 
     @commands.command(name="출석")
+    @commands.check(is_owner)
+    @commands.check(is_class)
     async def attendance(self, ctx):
-        if(ctx.author == ctx.guild.owner):
-            if str(ctx.channel) == "수업":
-                for member in ctx.channel.members:
-                    try:
-                        give_coin({"discord_id": member.id})
-                    except Exception as e:
-                        await ctx.send(f"오류로 인해 {member.name} 님의 알코인은 적립되지 않았습니다.")
-                        print(e)
-                    else:
-                        await ctx.send(f"{member.name} 님에게 100 알코인이 적립되었습니다.")
-
-                        
-                await ctx.send("출석체크 완료")
+        for member in ctx.channel.members:
+            try:
+                give_coin({"discord_id": member.id})
+            except Exception as e:
+                await ctx.send(f"오류로 인해 {member.name} 님의 알코인은 적립되지 않았습니다.")
+                print(e)
             else:
-                await ctx.send("수업 채널에서만 출석체크가 가능합니다.")
-        else:
+                await ctx.send(f"{member.name} 님에게 100 알코인이 적립되었습니다.")
+        await ctx.send("출석체크 완료")
+
+    @attendance.error
+    async def attendance_error(self, ctx, error):
+        if not is_owner(ctx):
             await ctx.send("선생님만 출석체크가 가능합니다.")
+        elif not is_class(ctx):
+            await ctx.send("수업 채널에서만 출석체크가 가능합니다.")
+        
+
 
 
 
